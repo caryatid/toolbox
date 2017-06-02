e25141a9b365e632f998edec2aaf9246068e7255
David Setchell <david.setchell@nwea.org>
2017-06-02 14:45:34 -0700

    sets up for gcc/musl build

--- boxin.sh
+++ boxin.sh
@@ -4,42 +4,74 @@ TMP=$(mktemp -d)
 trap "rm -rf $TMP" EXIT
 
 OGDIR=$(pwd)
-SDIR=dependency/repos
-IDIR=dependency/install
-RDATA=./repodata.csv
+SDIR="$OGDIR/dependency/repos"
+IDIR="$OGDIR/dependency/install"
+RDATA="$OGDIR/repodata.csv"
+I="" # can become "-i" to git 
 
 mkdir -p "$SDIR"; mkdir -p "$IDIR"
-cat <<'EOF' >$TMP/select_fmt
+cat <<'EOF' >$TMP/column_fmt
 BEGIN { FS="," }
 NR == 1 { for (i=1; i<=NF;i++) { if ($i == "%s") fnum = i }}
-NR > 1 { printf("%%-23s: %%s\\n", $1, $fnum) }
+NR > 1 { print  $fnum }
+EOF
+
+cat <<'EOF' >$TMP/value_fmt
+BEGIN { FS="," }
+NR == 1 { for (i=1; i<=NF;i++) { if ($i == "%s") fnum = i }}
+$1 == "%s" { print $fnum }
 EOF
 
 _list_fields () {
     head -n1 "$RDATA" | sed 's/,/ /g'
 }
 
-_get_by_field () {
+_get_column () { # column
     local field="${1:-name}"
-    printf "$(cat $TMP/select_fmt)" "$field" >$TMP/select
-    awk -f $TMP/select "$RDATA"
+    printf "$(cat $TMP/column_fmt)" "$field" >$TMP/column
+    awk -f $TMP/column "$RDATA"
 }
     
-get () {
-    local repo=${1:-
-    git clone $(_get_by_field "$repo")
+_get_value () { # name (id), column
+    test -z "$1" && return 1
+    local id="$1"; local column="${2:-internal-repo}"
+    printf "$(cat $TMP/value_fmt)" "$column" "$id" >$TMP/value
+    awk -f $TMP/value "$RDATA"
 }
 
-update_local () {
-    echo not implemented
+clone_repo () { 
+    test -z "$1" && return 1
+    local id="$1"; cd "$SDIR"
+    test -e "$id" && rm -Rf "$id"
+    git clone $(_get_value "$id" internal-repo) "$id"
+    cd "$id"
+    git checkout $(_get_value "$id" internal-branch)
+    git remote add -f og $(_get_value "$id" og-repo)
+    cd "$OGDIR"
 }
 
-reset_local () {
-    echo not implemented
+update_internal () {
+    test -z "$1" && return 1
+    local id="$1"; clone_repo "$id"; cd "$SDIR/$id"
+    git rebase $I og/$(_get_value "$id" og-branch) \
+        || { echo rebase fail; return 1 ;}
+    cd "$OGDIR"
+}
+
+reset_internal () {
+    test -z "$1" && return 1
+    local id="$1"; clone_repo "$id"; cd "$SDIR/$id"
+    git branch "$(date -I)-reset"; git fetch --all
+    git reset --hard og/$(_get_value "$id" og-branch)
+    git push --force
+    cd "$OGDIR"
 }
 
 build_env () {
-    echo not implemented
+    clone_repo musl
+    cd "$SDIR/musl"
+    ./configure --prefix="$IDIR" --disable-shared
+    make && make install   
 }
 
 build () {
@@ -54,4 +86,8 @@ remote () {
     echo not implemented
 }
 
-_get_by_field "$1"
+# _get_column "$2"
+# _get_value "$1" "$2"
+#clone_repo "$1"
+# reset_internal "$1"
+build_env
