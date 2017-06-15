646639718bce91701670e7ff15cba433073e0384
Dave Setchell <setchell.dave@gmail.com>
2017-06-14 19:43:45 -0700

    cleaning up the house

--- README.md
+++ README.md
@@ -2,4 +2,16 @@
 
 Build and deploy core tools.
 
-## Simple usage
+## commands
+
+### per repo
+fetch   // M source
+build   // fetch -> _patch -> _make -> M build
+install // build -> M install
+info    // String
+deps    // [dependencies]
+
+### global
+toolchain  // boostraps toolchain
+system     // builds system 
+remote     // remotes using system 
--- boxin.sh
+++ boxin.sh
@@ -3,23 +3,30 @@
 TMP=$(mktemp -d)
 trap "rm -rf $TMP" EXIT
 
-OGDIR=$(pwd)
-SDIR="$OGDIR/dependency/repos"
-IDIR="$OGDIR/dependency/install"
-RDATA="$OGDIR/repodata.csv"
-I="" # can become "-i" to git 
+# toolchain build order
+#   binutils
+#   gcc-temp
+#   musl
+#   gcc
 
-mkdir -p "$SDIR"; mkdir -p "$IDIR"
-cat <<'EOF' >$TMP/column_fmt
-BEGIN { FS="," }
-NR == 1 { for (i=1; i<=NF;i++) { if ($i == "%s") fnum = i }}
+PRE_D="$PWD"
+SRC_D="$PRE_D/src"
+INS_D="$PRE_D/ins"
+BLD_D="$PRE_D/bld"
+PCH_D="$PRE_D/pch"
+RDATA="$PRE_D/repodata.csv"
+mkdir -p "$SRC_D" "$INS_D" "$BLD_D" "$PCH_D"
+
+cat <<'EOF' >$TMP/column
+BEGIN { FS=","; COL=ARGV[2]; ARGV[2]=""}
+NR == 1 { for (i=1; i<=NF;i++) { if ($i == COL) fnum = i }}
 NR > 1 { print  $fnum }
 EOF
 
-cat <<'EOF' >$TMP/value_fmt
-BEGIN { FS="," }
-NR == 1 { for (i=1; i<=NF;i++) { if ($i == "%s") fnum = i }}
-$1 == "%s" { print $fnum }
+cat <<'EOF' >$TMP/value
+BEGIN { FS=","; COL=ARGV[3]; ROW=ARGV[2]; ARGV[3]=""; ARGV[2]="" }
+NR == 1 { for (i=1; i<=NF;i++) { if ($i == COL) fnum = i }}
+$1 == ROW { print $fnum }
 EOF
 
 _list_fields () {
@@ -28,26 +35,22 @@ _list_fields () {
 
 _get_column () { # column
     local field="${1:-name}"
-    printf "$(cat $TMP/column_fmt)" "$field" >$TMP/column
-    awk -f $TMP/column "$RDATA"
+    awk -f $TMP/column "$RDATA" "$field"
 }
     
-_get_value () { # name (id), column
+_get_value () {
     test -z "$1" && return 1
     local id="$1"; local column="${2:-internal-repo}"
-    printf "$(cat $TMP/value_fmt)" "$column" "$id" >$TMP/value
-    awk -f $TMP/value "$RDATA"
+    awk -f $TMP/value "$RDATA" "$id" "$column"
 }
 
 clone_repo () { 
     test -z "$1" && return 1
-    local id="$1"; cd "$SDIR"
+    local id="$1"; cd "$SRC_D"
     test -e "$id" && rm -Rf "$id"
-    git clone $(_get_value "$id" internal-repo) "$id"
+    git clone $(_get_value "$id" repo) "$id"
     cd "$id"
-    git checkout $(_get_value "$id" internal-branch)
-    git remote add -f og $(_get_value "$id" og-repo)
-    cd "$OGDIR"
+    git checkout $(_get_value "$id" branch)
 }
 
 update_internal () {
@@ -95,4 +98,7 @@ remote () {
 # _get_value "$1" "$2"
 #clone_repo "$1"
 # reset_internal "$1"
-build_env
+_list_fields
+_get_column "$@"
+_get_value st repo
+clone_repo binutils
