c6fae01c6264644be02c4c8b7bd7f5f0eb12cd50
David Setchell <david.setchell@nwea.org>
2017-06-29 23:34:50 +0000

    hostcc is key for netbsd

--- scripts/netbsd-curses.sh
+++ scripts/netbsd-curses.sh
@@ -4,9 +4,14 @@ TGT="$1"; shift
 
 set -e
 cd "$SRC"
+echo "PREFIX = $INS" >config.mak
+echo "TARGET = ${TGT}" >>config.mak
+cat <<'EOF' >>config.mak
+CFLAGS=-static -O2 -I. -I${PREFIX}/${TARGET}/include -I./libcurses -I${PREFIX}/include -nostdinc
+LDFLAGS=-static -L${PREFIX}/lib -L${PREFIX}/${TARGET}/lib -nodefaultlibs
+CC=${TARGET}-gcc
+HOSTCC=gcc
+EOF
 
-
-export CC=${TGT}-gcc
-export CFLAGS="-O2 -I. -I$INS/$TGT/include -I./libcurses -nostdinc"
-make PREFIX="$INS" all-static install-static
+make all-static install-static
 
