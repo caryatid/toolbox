6fa2c8d4eda76f16f1c1d4d3ce6ca107d23866a0
Dave Setchell <setchell.dave@gmail.com>
2017-07-03 20:27:33 -0700

    packaging

--- tb.sh
+++ tb.sh
@@ -5,7 +5,7 @@ trap "rm -rf $TMP" EXIT
 OG_D="$PWD"
 
 PRE_D="$PWD" 
-CODE_D=$(type -p "$0")
+CODE_D=$(cd $(dirname $(type -p "$0")); echo $PWD)
 FRESH=no
 
 SH_D="$CODE_D/scripts"
@@ -21,7 +21,8 @@ TARGET=x86_64-linux-musl
 
 export PATH="$INS_D/bin:$PATH"
 mkdir -p "$SRC_D" "$INS_D" "$BLD_D"
-cp "$EXT_D/*" "$INS_D" 
+cp $EXT_D/* "$INS_D" 
+
 
 cat <<'EOF' >$TMP/column
 BEGIN { FS=","; COL=ARGV[2]; ARGV[2]=""}
@@ -48,6 +49,10 @@ _get_value () {
     test -z "$1" && return 1
     local id="$1"; local column="${2:-internal-repo}"
     awk -f $TMP/value "$RDATA" "$id" "$column"
+}
+
+__ () {
+    test -n "$FOO" && eval "$@"
 }
 
 _loop_repos () {
@@ -123,22 +128,37 @@ shell_repo () {
     local src="$SRC_D/$id"
     test -d "$src" || return
     echo '--| entering' "$id" 'directory in a subshell |--'
-    cd "$src"; PS1_SUB="[$id]" $SHELL
+    cd "$src"; PS1_SUB="[$id]$PS1_SUB" $SHELL
 }
 
+
 activate () {
-    echo activates local machine
+    test -r "$1" || return 1
+    echo bzip2 -d \<"$1" \| tar -C $HOME -x
+    bzip2 -d <"$1" | tar -C $HOME -x
+    test -f "$HOME/.mkshrc" && mv "$HOME/.mkshrc" "$HOME/.mkshrc_back"
+    cp "$HOME/.toolbox/mkshrc" "$HOME/.mkshrc"
+    export SHELL=mksh
+    PS1_SUB="[tb]$PS1_SUB" $SHELL
 }
 
 package () {
-    echo foo
-    mkdir $PRE_D/.toolbox && cd "$PRE_D/.toolbox"
-    ln -s 
-    
-    sa.sh, mkshrc ( place or source from HOME ), toolbox itself
-    build "home" dir && tar it
-    put tar in location
+    mkdir -p "$PRE_D/.toolbox" 
+    cd "$PRE_D/.toolbox"
+    cp "$CODE_D/mkshrc" .
+    test -L bin || ln -s $INS_D/bin .
+    # TODO insert marker of date or id or something traceable
+    cd ..
+    tar -c .toolbox -h >tb.tar 
+    mv tb.tar .toolbox # recursive tar for history
+    <.toolbox/tb.tar bzip2 -c >tb.tar.bz 
+    echo "$PWD/tb.tar.bz"
 }
+
+    # 
+    # sa.sh, mkshrc ( place or source from HOME ), toolbox itself
+    # build "home" dir && tar it
+    # put tar in location
 
 remote () {
     # TODO will need to determine arch
@@ -165,18 +185,25 @@ cmd="$1"
 test -z "$cmd" && cmd=help || shift
 
 ### repo sets
-case "$1" in
-""|all)
-    _get_column >$TMP/rset
-    ;;
-toolchain)
-    echo ^mcm | tr ',' '\n' >$TMP/rset
-    ;;
-*)
-    echo "$1" | tr ',' '\n' >$TMP/rset
-    ;;
-esac
-_get_column name | grep -f $TMP/rset >$TMP/repos
+if $(_get_column name | grep -q "$(echo $1 | cut -d, -f1)")
+then
+    case "$1" in
+    ""|all)
+        shift
+        _get_column >$TMP/rset
+        ;;
+    toolchain)
+        shift
+        echo ^mcm | tr ',' '\n' >$TMP/rset
+        ;;
+    *)
+        echo "$1" | tr ',' '\n' >$TMP/rset
+        shift
+        ;;
+    esac
+    sed -i '/^[[:space:]]*$/d' $TMP/rset
+    _get_column name | grep -f $TMP/rset >$TMP/repos
+fi
 
 case "$cmd" in 
 fetch)
@@ -187,6 +214,12 @@ build)
     ;;
 shell)
     _loop_repos shell_repo $TMP/repos
+    ;;
+package)
+    package
+    ;;
+activate)
+    activate "$@" || echo fail
     ;;
 list)
     echo build order
