fce3a66ddf62be4b07643822509ff22d69e9f7bc
David Setchell <david.setchell@nwea.org>
2017-07-05 23:18:45 +0000

    flailing

--- scripts/gdb.sh
+++ scripts/gdb.sh
@@ -3,10 +3,8 @@ INS="$1"; shift
 TGT="$1"; shift
 
 set -e
+echo "$INS"; exit
 export CC=${TGT}-gcc
 export CXX=${TGT}-g++
-export LDFLAGS=-static
-# TODO fix version in prefix
-export GCC_EXEC_PREFIX=${INS}/libexec/gcc/${TGT}/6.3.0/
-# export GCC_EXEC_PREFIX=${INS}/libexec/gcc/
-$SRC/gdb/configure --enable-static  --prefix="$INS" --with-sysroot="$INS"
+export GCC_EXEC_PREFIX=${INS}/libexec/gcc/
+$SRC/gdb/configure --enable-static  --prefix="$INS" --with-sysroot="$INS" LDFLAGS="-static -L${INS}/lib/gcc/${TGT}/6.3.0"
