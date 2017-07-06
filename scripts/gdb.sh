ae1f35dca985f5b017c126d4bfd62946518c8452
Dave Setchell <setchell.dave@gmail.com>
2017-07-06 07:49:19 -0700

    gdb work

--- scripts/gdb.sh
+++ scripts/gdb.sh
@@ -3,8 +3,13 @@ INS="$1"; shift
 TGT="$1"; shift
 
 set -e
-echo "$INS"; exit
 export CC=${TGT}-gcc
 export CXX=${TGT}-g++
+export LIBRARY_PATH="${INS}/lib/gcc/${TGT}/6.3.0:${LIBRARY_PATH}"
+export LDFLAGS="-L${INS}/lib -L${INS}/${TGT}/lib -static -nodefaultlibs -L${INS}/lib/gcc/${TGT}/6.3.0"
+export INCS="-I. -nostdinc -I${INS}/include -I${INS}/${TGT}/include"
+export LIBS="-lc -lgcc"
+
 export GCC_EXEC_PREFIX=${INS}/libexec/gcc/
-$SRC/gdb/configure --enable-static  --prefix="$INS" --with-sysroot="$INS" LDFLAGS="-static -L${INS}/lib/gcc/${TGT}/6.3.0"
+$SRC/gdb/configure --enable-static  --prefix="$INS" \
+    --host=$TGT --with-sysroot=$INS --target=$TGT
