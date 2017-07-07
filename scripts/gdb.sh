f77216c1b35b9eb5b8bafda82908ce2e5165dd26
David Setchell <david.setchell@nwea.org>
2017-07-07 01:04:00 +0000

    foo

--- scripts/gdb.sh
+++ scripts/gdb.sh
@@ -5,11 +5,16 @@ TGT="$1"; shift
 set -e
 export CC=${TGT}-gcc
 export CXX=${TGT}-g++
-export LIBRARY_PATH="${INS}/lib/gcc/${TGT}/6.3.0:${LIBRARY_PATH}"
-export LDFLAGS="-L${INS}/lib -L${INS}/${TGT}/lib -static -nodefaultlibs -L${INS}/lib/gcc/${TGT}/6.3.0"
-export INCS="-I. -nostdinc -I${INS}/include -I${INS}/${TGT}/include"
-export LIBS="-lc -lgcc"
+#_d="$PWD"
+#cd "$SRC"
+#./configure --enable-static  --prefix="$INS" --with-sysroot=${INS}
+#cd "$_d"
+# export LIBRARY_PATH="${INS}/lib/gcc/${TGT}/6.3.0:${LIBRARY_PATH}"
+#export LDFLAGS="-L${INS}/lib -L${INS}/${TGT}/lib -static -nodefaultlibs"
 
-export GCC_EXEC_PREFIX=${INS}/libexec/gcc/
-$SRC/gdb/configure --enable-static  --prefix="$INS" \
-    --host=$TGT --with-sysroot=$INS --target=$TGT
+# export GCC_EXEC_PREFIX=${INS}/libexec/gcc/
+# export CFLAGS="-I. -nostdinc -I${INS}/include -I${INS}/${TGT}/include"
+# export CXXFLAGS="-I. -nostdinc -I${INS}/include -I${INS}/${TGT}/include"
+export LDFLAGS="-static"
+$SRC/gdb/configure --enable-static  --prefix="$INS" --with-sysroot=${INS} --host=$TGT --target=${TGT} --build=${TGT}
+make
